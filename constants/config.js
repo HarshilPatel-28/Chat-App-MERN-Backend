206b008c09393873b9904271cefa481c5558ab9d
Harshil Patel <hp950036@gmail.com>
2024-07-02 22:19:01 +0530

    Bug fixed

--- constants/config.js
+++ constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
     origin: [
       "http://localhost:5173",
       "http://localhost:4173",
+      'https://chat-app-mern-frontend-git-main-harshilpatel28s-projects.vercel.app',
       process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
