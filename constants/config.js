ee27279d844797aadef32b1ecb869a16416b280f
Harshil Patel <hp950036@gmail.com>
2024-07-03 17:26:24 +0530

    Origin fixed

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,9 @@
 const corsOptions = {
-    origin: 
+    origin: [
       process.env.CLIENT_URL,
-      
+      "http://localhost:5173",
+      "http://localhost:4173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   };
